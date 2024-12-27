218da25f303e09c37fb65291c0a021d136e222f7
Chris C <chriscortese1984@gmail.com>
2024-12-26 20:14:00 -0500

    test making any change because container failed

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -116,6 +116,7 @@ namespace UFCApp.Areas.Identity.Pages.Account
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
+
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
